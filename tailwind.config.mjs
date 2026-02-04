14ee8dc58c9a7f2b200e9dc0023d083a823ace97
Keshav <keshav.nc30@gmail.com>
2026-02-05 00:12:46 +0530

    letter changes

---
+++ tailwind.config.mjs
@@ -0,0 +1,31 @@
+// /** @type {import('tailwindcss').Config} */
+// export const content = ["./*.html"];
+// export const theme = {
+//   extend: {
+//     keyframes: {
+//       "slide-right": {
+//         "0%": { transform: "translateX(-100%)" },
+//         "100%": { transform: "translateX(0%)" },
+//       },
+//       "slide-left": {
+//         "0%": { transform: "translateX(200%)" },
+//         "100%": { transform: "translateX(0%)" },
+//       },
+//       "slide-up": {
+//         "0%": { transform: "translateY(200%)" },
+//         "100%": { transform: "translateY(0%)" },
+//       },
+//       "slide-down": {
+//         "0%": { transform: "translateY(-200%)" },
+//         "100%": { transform: "translateY(0%)" },
+//       },
+//     },
+//     animation: {
+//       "slide-right": "slide-right 1s ease-out forwards",
+//       "slide-left": "slide-left 1s ease-out forwards",
+//       "slide-up": "slide-up 1s ease-out forwards",
+//       "slide-down": "slide-down 1s ease-out forwards",
+//     },
+//   },
+// };
+// export const plugins = [];
